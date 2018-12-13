4a9db2799b2c7b9e4be13611aaa7b88745226068
Morten Henriksen <mh@gi2.dk>
2018-12-13 21:31:26 +0100

    bump 1.0.0 - underscore removed

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "raix:eventemitter",
-  version: '0.1.3',
+  version: '1.0.0',
   summary: "A server and client event package.",
   git: "https://github.com/raix/Meteor-EventEmitter.git"
 });
