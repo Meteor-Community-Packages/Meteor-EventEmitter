7d3c573ac4b0d15db486f11a1f13efcb80b16217
jankapunkt <jkuester@uni-bremen.de>
2024-08-21 08:18:43 +0200

    prepaere release 2.0.0

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "raix:eventemitter",
-  version: '2.0.0-rc.0',
+  version: '2.0.0',
   summary: "A server and client event package.",
   git: "https://github.com/raix/Meteor-EventEmitter.git",
   deprecated: true
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom(['1.0', '2.8.0', '3.0-rc.4']);
+  api.versionsFrom(['1.0', '2.8.0', '3.0']);
   api.addFiles('eventemitter.client.js', 'client');
   api.addFiles('eventemitter.server.js', 'server');
   api.export('EventEmitter');
@@ -15,7 +15,7 @@ Package.onUse(function (api) {
 
 
 Package.onTest(function (api) {
-  api.versionsFrom(['1.0', '2.8.0', '3.0-rc.4']);
+  api.versionsFrom(['1.0', '2.8.0', '3.0']);
   api.use(['raix:eventemitter']);
   api.use('test-helpers', ['server', 'client']);
   api.use('tinytest');
